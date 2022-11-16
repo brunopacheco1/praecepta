3dd0c2bfdadcd0954a582087aa351eff0f21f6b4
Bruno Pacheco <brunopacheco1@yahoo.com>
2022-11-16 21:53:27 +0100

    Enable Github Actions and SonarCloud

--- src/main/java/com/github/brunopacheco1/praecepta/Output.java
+++ src/main/java/com/github/brunopacheco1/praecepta/Output.java
@@ -3,5 +3,5 @@ package com.github.brunopacheco1.praecepta;
 import java.util.Map;
 
 public final record Output(
-                Map<String, String> values) {
+        Map<String, String> values) {
 }
--- src/main/java/com/github/brunopacheco1/praecepta/Praeceptum.java
+++ src/main/java/com/github/brunopacheco1/praecepta/Praeceptum.java
@@ -1,7 +1,7 @@
 package com.github.brunopacheco1.praecepta;
 
 public final record Praeceptum(
-                Integer priority,
-                Input input,
-                Output output) {
+        Integer priority,
+        Input input,
+        Output output) {
 }
