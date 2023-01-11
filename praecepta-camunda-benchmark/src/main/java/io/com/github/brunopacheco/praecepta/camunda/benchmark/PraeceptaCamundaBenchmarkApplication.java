6fb83cfd011c49ca550bcaf6caae3a127e2298d7
Bruno PACHECO <bpacheco@pictet-technologies.com>
2023-01-11 22:49:17 +0100

    Add simple springboot app

---
+++ praecepta-camunda-benchmark/src/main/java/io/com/github/brunopacheco/praecepta/camunda/benchmark/PraeceptaCamundaBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package io.com.github.brunopacheco.praecepta.camunda.benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraeceptaCamundaBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraeceptaCamundaBenchmarkApplication.class, args);
+	}
+
+}
---
+++ praecepta-camunda-benchmark/src/test/java/io/com/github/brunopacheco/praecepta/camunda/benchmark/PraeceptaCamundaBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.github.brunopacheco.praecepta.camunda.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraeceptaCamundaBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
